1da0f81103e62ec19ed23bdde1a65ee67eddf25b
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-13 14:31:45 +0200

    create CreateOrder compo and initialize with basic setups

---
+++ src/features/order/CreateOrder.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+
+// https://uibakery.io/regex-library/phone-number
+const isValidPhone = (str: string): boolean =>
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str
+  );
+
+type CartItem = {
+  pizzaId: number;
+  name: string;
+  quantity: number;
+  unitPrice: number;
+  totalPrice: number;
+};
+
+const fakeCart: CartItem[] = [
+  {
+    pizzaId: 12,
+    name: "Mediterranean",
+    quantity: 2,
+    unitPrice: 16,
+    totalPrice: 32,
+  },
+  {
+    pizzaId: 6,
+    name: "Vegetable",
+    quantity: 1,
+    unitPrice: 13,
+    totalPrice: 13,
+  },
+  {
+    pizzaId: 11,
+    name: "Spinach and Mushroom",
+    quantity: 1,
+    unitPrice: 15,
+    totalPrice: 15,
+  },
+];
+
+const CreateOrder: React.FC = () => {
+  // const [withPriority, setWithPriority] = useState<boolean>(false);
+  const cart = fakeCart;
+
+  return (
+    <div>
+      <h2>Ready to order? Let's go!</h2>
+
+      <form>
+        <div>
+          <label>First Name</label>
+          <input type="text" name="customer" required />
+        </div>
+
+        <div>
+          <label>Phone number</label>
+          <div>
+            <input type="tel" name="phone" required />
+          </div>
+        </div>
+
+        <div>
+          <label>Address</label>
+          <div>
+            <input type="text" name="address" required />
+          </div>
+        </div>
+
+        <div>
+          <input
+            type="checkbox"
+            name="priority"
+            id="priority"
+            // value={withPriority}
+            // onChange={(e) => setWithPriority(e.target.checked)}
+          />
+          <label htmlFor="priority">Want to yo give your order priority?</label>
+        </div>
+
+        <div>
+          <button>Order now</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateOrder;
