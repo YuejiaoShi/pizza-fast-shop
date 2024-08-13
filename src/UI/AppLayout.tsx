a405d0916432c4e425541e2bacd3a95a3bcc71af
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-13 18:24:54 +0200

    use Outlet from react-router to render all the nested route inside child compo of  AppLayout

--- src/UI/AppLayout.tsx
+++ src/UI/AppLayout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
+import { Outlet } from "react-router-dom";
 
 const AppLayout: React.FC = () => {
   return (
@@ -8,6 +9,7 @@ const AppLayout: React.FC = () => {
       <Header />
       <main>
         <h1>Content</h1>
+        {/* <Outlet /> */}
       </main>
       <CartOverview />
     </div>
