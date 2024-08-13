588d8ab3d4fd3f150698ec28f7305c6b4f9f1086
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-13 21:44:41 +0200

    create Loader with basic setup

---
+++ src/UI/Loader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface LoaderProps {
+  // define props here
+}
+
+const Loader: React.FC<LoaderProps> = ({}) => {
+  return <div className="loader">// component content</div>;
+};
+
+export default Loader;
