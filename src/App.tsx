ba6d15f5c312d5c611b6c9b6e6f504301fe3849c
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-13 17:45:07 +0200

    createBrowserRouter for Home and Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./UI/Home";
+import Menu from "./features/menu/Menu";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+]);
 
 const App: React.FC = () => {
-  return (
-    <>
-
-    </>
-  );
-}
+  return <RouterProvider router={router}></RouterProvider>;
+};
 
 export default App;
