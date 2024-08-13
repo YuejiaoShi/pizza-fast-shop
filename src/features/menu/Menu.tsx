da72fede7e5d764ef35aaec93596df797d374e52
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-13 21:28:42 +0200

    map over the pizzas in menu; manage some types

--- src/features/menu/Menu.tsx
+++ src/features/menu/Menu.tsx
@@ -1,15 +1,22 @@
 import React from "react";
-import { getMenu, MenuItem } from "../../servers/apiRestaurant";
+import { getMenu, type Pizza } from "../../servers/apiRestaurant";
 import { useLoaderData } from "react-router-dom";
+import MenuItem from "./MenuItem";
 
 const Menu: React.FC = () => {
-  const menu = useLoaderData();
+  const menu = useLoaderData() as Pizza[];
   console.log(menu);
-  return <h1>Menu</h1>;
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 };
 
 // loader function to fetch the menu data
-export const loader = async (): Promise<MenuItem[]> => {
+export const loader = async (): Promise<Pizza[]> => {
   const menu = await getMenu();
   return menu;
 };
