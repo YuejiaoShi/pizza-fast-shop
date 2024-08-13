eb3c2ab598e7b300a0b00e8c9ed438a8235806af
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-13 21:58:48 +0200

    successfully show the error message on Error Page

--- src/UI/Error.tsx
+++ src/UI/Error.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { useNavigate, useRouteError } from "react-router-dom";
 
+interface ErrorResponseImpl {
+  data?: string;
+}
+
 const Error: React.FC = () => {
   const navigate = useNavigate();
-  const error = useRouteError();
-  console.log(error);
+  const error = useRouteError() as ErrorResponseImpl;
+
   return (
     <div>
       <h1>Something went wrong 😢</h1>
-      <p>%MESSAGE%</p>
+      <p>{error.data}</p>
       <button onClick={() => navigate(-1)}>&larr; Go back</button>
     </div>
   );
