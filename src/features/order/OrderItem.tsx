3f4f8085c92f473f85d3c32a0d6056c27ed55be5
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-13 15:13:20 +0200

    create OrderItem compo and initialize with basic setups

---
+++ src/features/order/OrderItem.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+type OrderItemProps = {
+  item: {
+    quantity: number;
+    name: string;
+    totalPrice: number;
+  };
+  isLoadingIngredients: boolean;
+  ingredients: string[];
+};
+
+const OrderItem: React.FC<OrderItemProps> = ({
+  item,
+  isLoadingIngredients,
+  ingredients,
+}) => {
+  const { quantity, name, totalPrice } = item;
+
+  return (
+    <li>
+      <div>
+        <p>
+          <span>{quantity}&times;</span> {name}
+        </p>
+        <p>{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
+};
+
+export default OrderItem;
