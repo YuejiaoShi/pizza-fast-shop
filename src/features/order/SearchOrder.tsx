b6c8ae8fec30715e79c4238c968a8ce519f9e652
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-13 22:26:19 +0200

    when handleSubmit triggered, navigate to the order page

--- src/features/order/SearchOrder.tsx
+++ src/features/order/SearchOrder.tsx
@@ -1,12 +1,19 @@
 import React, { FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface SearchOrderProps {}
 
 const SearchOrder: React.FC<SearchOrderProps> = () => {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState<string>("");
+  const navigate = useNavigate();
+
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery("");
   }
+
   return (
     <form onSubmit={handleSubmit}>
       <input
