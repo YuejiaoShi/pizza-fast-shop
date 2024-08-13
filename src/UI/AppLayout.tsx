2760ea570e2e6c5ef1ffbcc5e449cf84634bad56
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-13 21:42:46 +0200

    format file

--- src/UI/AppLayout.tsx
+++ src/UI/AppLayout.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const AppLayout: React.FC = () => {
-  const navigation = useNavigation();
-
-  const isLoading = navigation.state === "loading";
-  console.log(navigation, isLoading);
   return (
     <div>
       <Header />
