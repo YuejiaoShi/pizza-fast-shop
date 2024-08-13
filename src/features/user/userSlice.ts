fbb877beca4f979b1486ea87531d7661dfcbe560
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-13 15:19:25 +0200

    create userSlice compo and initialize with basic setups

---
+++ src/features/user/userSlice.ts
@@ -0,0 +1,45 @@
+type Position = {
+  latitude: number;
+  longitude: number;
+};
+
+type Address = {
+  locality?: string;
+  city?: string;
+  postcode?: string;
+  countryName?: string;
+};
+
+// Define the type for the position object returned by geolocation API
+type GeolocationPosition = {
+  coords: {
+    latitude: number;
+    longitude: number;
+  };
+};
+
+export function getPosition(): Promise<GeolocationPosition> {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
+
+export async function fetchAddress(): Promise<{
+  position: Position;
+  address: string;
+}> {
+  // 1) Get the user's geolocation position
+  // 1) We get the user's geolocation position
+  const positionObj = await getPosition();
+  const position = {
+    latitude: positionObj.coords.latitude,
+    longitude: positionObj.coords.longitude,
+  };
+
+  // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+  const addressObj = await getAddress(position);
+  const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+
+  // 3) Then we return an object with the data that we are interested in
+  return { position, address };
+}
