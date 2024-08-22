b93e6d8dd8d117cb700d6fe3dd0ba8f623a8f4b2
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-22 22:44:24 +0200

    add padding

--- src/features/cart/CartOverview.tsx
+++ src/features/cart/CartOverview.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const CartOverview: React.FC = () => {
   return (
-    <div className="bg-stone-800 text-stone-200 uppercase">
+    <div className="bg-stone-800 text-stone-200 uppercase p-4">
       <p className="text-stone-300 font-semibold ">
         <span>23 pizzas</span>
         <span>$23.45</span>
