815fe7a85e553fcbd5c3e5018b5fab93733c72b5
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-22 22:43:03 +0200

    add border

--- src/UI/Header.tsx
+++ src/UI/Header.tsx
@@ -6,7 +6,7 @@ import UserName from "../features/user/userName";
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-amber-500 uppercase px-4 py-3">
+    <header className="bg-amber-500 uppercase px-4 py-3 border-b-4 border-stone-300">
       <Link to="/" className="tracking-widest">
         Pizza Fast Denmark A/S
       </Link>
