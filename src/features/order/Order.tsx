6ef5d32899520f53e7b476217b697043d72455ef
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-22 17:10:17 +0200

    remove fake order example

--- src/features/order/Order.tsx
+++ src/features/order/Order.tsx
@@ -10,41 +10,6 @@ import {
 import { getOrder, type OrderType } from "../../servers/apiRestaurant";
 import { LoaderFunctionArgs, useLoaderData } from "react-router-dom";
 
-// const order = {
-//   id: "ABCDEF",
-//   customer: "Jonas",
-//   phone: "123456789",
-//   address: "Arroios, Lisbon , Portugal",
-//   priority: true,
-//   estimatedDelivery: "2027-04-25T10:00:00",
-//   cart: [
-//     {
-//       pizzaId: 7,
-//       name: "Napoli",
-//       quantity: 3,
-//       unitPrice: 16,
-//       totalPrice: 48,
-//     },
-//     {
-//       pizzaId: 5,
-//       name: "Diavola",
-//       quantity: 2,
-//       unitPrice: 16,
-//       totalPrice: 32,
-//     },
-//     {
-//       pizzaId: 3,
-//       name: "Romana",
-//       quantity: 1,
-//       unitPrice: 15,
-//       totalPrice: 15,
-//     },
-//   ],
-//   position: "-9.000,38.000",
-//   orderPrice: 95,
-//   priorityPrice: 19,
-// };
-
 const Order: React.FC = () => {
   const order = useLoaderData() as OrderType;
   // Everyone can search for all orders, so for privacy reasons we're gonna
