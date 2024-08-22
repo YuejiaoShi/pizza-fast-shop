00770a50b00bd41ae5893c691d512a8d8de46c94
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-22 22:36:37 +0200

    add a UserName compo and include in Header

--- src/UI/Header.tsx
+++ src/UI/Header.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import UserName from "../features/user/userName";
+
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-amber-500">
-      <Link to="/">PizzaFast Co.</Link>;
+    <header className="bg-amber-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Pizza Fast Denmark A/S
+      </Link>
       <SearchOrder />
+      <UserName />
     </header>
   );
 };
---
+++ src/features/user/userName.tsx
@@ -0,0 +1,5 @@
+const UserName = () => {
+  return <div className="text-sm font-semibold">Yuejiao</div>;
+};
+
+export default UserName ;
