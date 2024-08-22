981a0c6e9d0035a446831f0e4d08740261fc0754
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-22 22:40:10 +0200

    add margin

--- src/UI/Home.tsx
+++ src/UI/Home.tsx
@@ -1,15 +1,17 @@
 import React from "react";
+import CreateUser from "../features/user/CreateUser";
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1 className="text-xl font-semibold text-center">
+    <div className="my-10 text-center">
+      <h1 className="text-xl font-semibold text-center mb-8">
         The best pizza.
         <br />
         <span className="text-amber-500">
           Straight out of the oven, straight to you.
         </span>
       </h1>
+      <CreateUser/>
     </div>
   );
 };
