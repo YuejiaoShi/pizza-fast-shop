12f0e973c3b8cb55b5b59de731f74f887a8ec7b8
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-23 23:16:35 +0200

    style the paragraphs

--- src/features/cart/CartItem.tsx
+++ src/features/cart/CartItem.tsx
@@ -14,11 +14,11 @@ const CartItem: React.FC<CartItemProps> = ({ item }) => {
   const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
-    <li className="py-3">
+    <li className="py-3 sm:flex sm:items-center sm:justify-between">
       <p>
         {quantity}&times; {name}
       </p>
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
         <Button type="small">Delete</Button>
       </div>
