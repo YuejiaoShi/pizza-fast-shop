212b2d11ffaafd75c1aef919c5198095b34ab47c
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-23 20:32:29 +0200

    define own class .input and apply it

--- src/features/order/CreateOrder.tsx
+++ src/features/order/CreateOrder.tsx
@@ -63,13 +63,13 @@ const CreateOrder: React.FC = () => {
       <Form method="POST" action="/order/new">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input type="text" name="customer" required className="input" />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input type="tel" name="phone" required className="input" />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -77,12 +77,7 @@ const CreateOrder: React.FC = () => {
         <div>
           <label>Address</label>
           <div>
-            <input
-              type="text"
-              name="address"
-              required
-              className="rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-amber-500 w-full md:px-6 md:py-3"
-            />
+            <input type="text" name="address" required className="input" />
           </div>
         </div>
 
