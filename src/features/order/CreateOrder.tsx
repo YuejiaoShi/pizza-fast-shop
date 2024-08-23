ee296ed7d280cf0195c3f4f27934a89bf3442e7d
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-23 20:40:17 +0200

    utilize Button for 'order now' button with all the class in it

--- src/features/order/CreateOrder.tsx
+++ src/features/order/CreateOrder.tsx
@@ -5,6 +5,7 @@ import {
 } from "../../servers/apiRestaurant";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import React, { useState } from "react";
+import Button from "../../UI/Button";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str: string): boolean =>
@@ -95,12 +96,9 @@ const CreateOrder: React.FC = () => {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)}></input>
-          <button
-            disabled={isSubmitting}
-            className="bg-amber-500 text-stone-800 py-3 px-4 font-semibold uppercase tracking-wide rounded-full hover:bg-amber-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-amber-300 focus:bg-amber-300 focus:ring-offset-2 disabled:cursor-not-allowed"
-          >
+          <Button disabled={isSubmitting}>
             {isSubmitting ? "Placing order..." : "Order now"}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
