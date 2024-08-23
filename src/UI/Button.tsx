6dc06bb26a890d02b6333bca63d026c4e9196d23
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-23 21:11:00 +0200

    update Button to handle the link-to redirection

--- src/UI/Button.tsx
+++ src/UI/Button.tsx
@@ -1,16 +1,25 @@
+import { Link } from "react-router-dom";
 import React from "react";
 
 interface ButtonProps {
   children: React.ReactNode;
   disabled?: boolean;
+  to?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, disabled = false }) => {
+const className =
+  "bg-amber-500 text-stone-800 py-3 px-4 font-semibold uppercase tracking-wide rounded-full hover:bg-amber-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-amber-300 focus:bg-amber-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px=6 sm:py-4";
+
+const Button: React.FC<ButtonProps> = ({ children, disabled = false, to }) => {
+  if (to)
+    return (
+      <Link className={className} to={to}>
+        {children}
+      </Link>
+    );
+
   return (
-    <button
-      disabled={disabled}
-      className="bg-amber-500 text-stone-800 py-3 px-4 font-semibold uppercase tracking-wide rounded-full hover:bg-amber-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-amber-300 focus:bg-amber-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px=6 sm:py-4"
-    >
+    <button disabled={disabled} className={className}>
       {children}
     </button>
   );
