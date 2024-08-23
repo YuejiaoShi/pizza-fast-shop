6cf878f1db84c9dde690aa140ef2221d857f4bd7
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-23 21:29:26 +0200

    remove font-pizza cause it not exited

--- src/UI/Header.tsx
+++ src/UI/Header.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-amber-500 uppercase px-4 py-3 border-b-4 border-stone-300 sm:px-6 flex items-center justify-between font-pizza">
+    <header className="bg-amber-500 uppercase px-4 py-3 border-b-4 border-stone-300 sm:px-6 flex items-center justify-between">
       <Link to="/" className="tracking-widest">
         Pizza Fast Denmark A/S
       </Link>
