dda39c31e81323dba75ce7e8155d1dcb96c4223d
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-23 21:40:45 +0200

    use flex to list of pizza at Menu; add divider and padding between each

--- src/features/menu/Menu.tsx
+++ src/features/menu/Menu.tsx
@@ -1,12 +1,12 @@
-import React from "react";
 import { getMenu, type PizzaType } from "../../servers/apiRestaurant";
 import { useLoaderData } from "react-router-dom";
 import MenuItem from "./MenuItem";
+import React from "react";
 
 const Menu: React.FC = () => {
   const menu = useLoaderData() as PizzaType[];
   return (
-    <ul>
+    <ul className="divide-y divide-stone-200 px-2">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
--- src/features/menu/MenuItem.tsx
+++ src/features/menu/MenuItem.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { formatCurrency } from "../../utilities/helpers";
 import { PizzaType } from "../../servers/apiRestaurant";
+import React from "react";
 
 export type MenuItemProps = {
   pizza: PizzaType;
@@ -10,13 +10,19 @@ const MenuItem: React.FC<MenuItemProps> = ({ pizza }) => {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
+    <li className="flex gap-4 py-2">
+      <img src={imageUrl} alt={name} className="h-24" />
+      <div className="flex flex-col">
         <p>{name}</p>
         <p>{ingredients.join(", ")}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+        <div className="mt-auto">
+          {!soldOut ? (
+            <p className="text-sm uppercase font-medium text-stone-500">
+              {formatCurrency(unitPrice)}
+            </p>
+          ) : (
+            <p>Sold out</p>
+          )}
         </div>
       </div>
     </li>
