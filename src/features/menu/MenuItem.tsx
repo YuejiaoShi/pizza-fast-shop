d1b18cc58681593ad03eb178ab3bb6c627bb0b8f
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-23 21:44:48 +0200

    conditional styling img; style some texts

--- src/features/menu/MenuItem.tsx
+++ src/features/menu/MenuItem.tsx
@@ -11,17 +11,23 @@ const MenuItem: React.FC<MenuItemProps> = ({ pizza }) => {
 
   return (
     <li className="flex gap-4 py-2">
-      <img src={imageUrl} alt={name} className="h-24" />
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
+      />
       <div className="flex flex-col">
-        <p>{name}</p>
-        <p>{ingredients.join(", ")}</p>
+        <p className="font-medium">{name}</p>
+        <p className="text-sm italic text-stone-500">
+          {ingredients.join(", ")}
+        </p>
         <div className="mt-auto">
           {!soldOut ? (
-            <p className="text-sm uppercase font-medium text-stone-500">
-              {formatCurrency(unitPrice)}
-            </p>
+            <p className="text-sm">{formatCurrency(unitPrice)}</p>
           ) : (
-            <p>Sold out</p>
+            <p className="text-sm uppercase font-medium text-stone-500">
+              Sold out
+            </p>
           )}
         </div>
       </div>
