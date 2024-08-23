eda57bd75952dcc905308b05facfa0f554caeb19
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-23 19:40:52 +0200

    add class to style input

--- src/features/order/SearchOrder.tsx
+++ src/features/order/SearchOrder.tsx
@@ -18,9 +18,10 @@ const SearchOrder: React.FC = () => {
         placeholder="Search Order..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="rounded-full px-4 py-2 text-sm bg-amber-100 placeholder:text-stone-400 w-28 focus:w-72 sm:w-64 transition-all duration-300 focus:outline-none focus:ring focus:ring-amber-500 focus:ring-opacity-50"
       ></input>
     </form>
-  );
+  );  
 };
 
 export default SearchOrder;
