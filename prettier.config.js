02526a08735cec51482326a7920bc5c9cfc4d0bb
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-23 19:33:41 +0200

    update prettier settings

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,3 @@
 export default {
-  printWidth: 80,
-  tabWidth: 2,
-  singleQuote: true,
-  trailingComma: 'all',
-  semi: true,
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
   plugins: ['prettier-plugin-sort-imports'],
 };
