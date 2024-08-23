3c9f493ecdce2263d9e5d6a431e1ead03c1eab24
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-23 18:26:36 +0200

    adjust text and padding styles

--- src/UI/Home.tsx
+++ src/UI/Home.tsx
@@ -3,8 +3,8 @@ import CreateUser from "../features/user/CreateUser";
 
 const Home: React.FC = () => {
   return (
-    <div className="my-10 text-center sm:my-16">
-      <h1 className="text-xl font-semibold text-center mb-8">
+    <div className="my-10 text-center sm:my-16 px-4">
+      <h1 className="text-xl font-semibold text-center mb-8 md:text-3xl">
         The best pizza.
         <br />
         <span className="text-amber-500">
