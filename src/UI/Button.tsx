ba5a5b73dfb8cac326d7e50e77635ff93613daf4
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-23 20:51:26 +0200

    add class: sm:px=6 sm:py-4

--- src/UI/Button.tsx
+++ src/UI/Button.tsx
@@ -9,7 +9,7 @@ const Button: React.FC<Props> = ({ children, disabled = false }) => {
   return (
     <button
       disabled={disabled}
-      className="bg-amber-500 text-stone-800 py-3 px-4 font-semibold uppercase tracking-wide rounded-full hover:bg-amber-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-amber-300 focus:bg-amber-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+      className="bg-amber-500 text-stone-800 py-3 px-4 font-semibold uppercase tracking-wide rounded-full hover:bg-amber-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-amber-300 focus:bg-amber-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px=6 sm:py-4"
     >
       {children}
     </button>
