8543b1ee0f6f52f013be14a0fed5b933190871bc
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-24 23:18:05 +0200

    read username data from Redux store

--- src/UI/Home.tsx
+++ src/UI/Home.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import CreateUser from "../features/user/CreateUser";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
 
 const Home: React.FC = () => {
+  const username = useSelector((state: RootState) => state.user.username);
+
   return (
     <div className="my-10 text-center sm:my-16 px-4">
       <h1 className="text-xl font-semibold text-center mb-8 md:text-3xl">
@@ -11,7 +15,7 @@ const Home: React.FC = () => {
           Straight out of the oven, straight to you.
         </span>
       </h1>
-      <CreateUser/>
+      <CreateUser />
     </div>
   );
 };
