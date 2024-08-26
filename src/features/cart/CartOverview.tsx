a80c4934a832e35dfc9717875721421c79163160
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-26 22:01:03 +0200

    use 'useSelector' function to read cart data

--- src/features/cart/CartOverview.tsx
+++ src/features/cart/CartOverview.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { RootState } from "../../store";
+import React from "react";
 
 const CartOverview: React.FC = () => {
+  const cart = useSelector((state: RootState) => state.cart.cart);
+
   return (
     <div className="bg-stone-800 text-stone-200 uppercase p-4 text-sm md:text-base sm:px-6 flex items-center justify-between">
       <p className="text-stone-300 font-semibold sm:space-x-6">
