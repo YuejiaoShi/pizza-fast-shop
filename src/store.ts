edff39daf413f29ba82d30ef4f05a5dd26cdef86
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-26 21:27:54 +0200

    connect cartSlice to store.ts

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/user/userSlice";
+import cartReducer from "./features/cart/cartSlice";
+import { configureStore } from "@reduxjs/toolkit";
 
-const store = configureStore({ reducer: { user: userReducer } });
+const store = configureStore({
+  reducer: { user: userReducer, cart: cartReducer },
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 
