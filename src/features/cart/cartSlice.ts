839ddf5bb525ff1ff266d40c30da4a40f18edbe6
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-26 21:14:56 +0200

    list main reducers of cartSlice

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,3 +1,4 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { CartItem } from "./Cart";
 
 type initialStateType = {
@@ -7,3 +8,17 @@ type initialStateType = {
 const initialState: initialStateType = {
   cart: [],
 };
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action: PayloadAction<string>) {},
+    deleteItem(state, actio: PayloadAction<string>) {},
+    increaseItemQuantity(state, action: PayloadAction<string>) {},
+    decreaseItemQuantity(state, action: PayloadAction<string>) {},
+    clearCart(state, action: PayloadAction<string>) {},
+  },
+});
+
+export default cartSlice.reducer;
