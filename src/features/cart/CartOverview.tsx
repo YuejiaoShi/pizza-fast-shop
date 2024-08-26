56f34e2e2b2c793576a5401062f3c46fc51f613f
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-26 22:03:48 +0200

    display totalCartQuantity

--- src/features/cart/CartOverview.tsx
+++ src/features/cart/CartOverview.tsx
@@ -11,7 +11,7 @@ const CartOverview: React.FC = () => {
   return (
     <div className="bg-stone-800 text-stone-200 uppercase p-4 text-sm md:text-base sm:px-6 flex items-center justify-between">
       <p className="text-stone-300 font-semibold sm:space-x-6">
-        <span>23 pizzas</span>
+        <span>{totalCartQuantity} pizzas</span>
         <span>$23.45</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
