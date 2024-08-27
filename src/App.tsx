50f1bdce08f4c772dd91a36cab6b601781dab9c2
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-27 19:18:47 +0200

    connect the action

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./UI/Home";
-import Menu, { loader as MenuLoader } from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
 import CreateOrder, {
   action as createOrderAction,
 } from "./features/order/CreateOrder";
+import { action as UpdateOrderAction } from "./features/order/UpdateOrder";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Order, { loader as OrderLoader } from "./features/order/Order";
+import Menu, { loader as MenuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
 import AppLayout from "./UI/AppLayout";
 import Error from "./UI/Error";
+import Home from "./UI/Home";
+import React from "react";
 
 const router = createBrowserRouter([
   {
@@ -34,6 +35,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: OrderLoader,
         errorElement: <Error />,
+        action: UpdateOrderAction,
       },
       {
         path: "/order/new",
