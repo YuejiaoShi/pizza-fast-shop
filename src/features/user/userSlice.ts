698c2107aba0167c21311d5674be8bf75b067fc9
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-28 18:02:46 +0200

    remove unused 'action'

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -62,7 +62,7 @@ const userSlice = createSlice({
   },
   extraReducers: (builder) =>
     builder
-      .addCase(fetchAddress.pending, (state, action) => {
+      .addCase(fetchAddress.pending, (state) => {
         state.status = "loading";
       })
       .addCase(fetchAddress.fulfilled, (state, action) => {
@@ -70,7 +70,7 @@ const userSlice = createSlice({
         state.address = action.payload.address;
         state.status = "idle";
       })
-      .addCase(fetchAddress.rejected, (state, action) => {
+      .addCase(fetchAddress.rejected, (state) => {
         state.status = "error";
         state.error = "Fail to get your location :(";
       }),
